31744cbfc29b4f75c41279aee6fe0d80fdf9645f
unknown <mr.kat2let@gmail.com>
2022-05-09 00:18:20 +0630

    initalize github repository

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTA5NzU0NzM2Nzg4NTA0NTg2.YZI5Hw.igxkm5b1e-JWtQug8e-TKhYpRWU"
-
 const client = new Discord.Client({
     intents:[
         "GUILDS",
